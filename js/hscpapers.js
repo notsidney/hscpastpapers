1720439d3d58b24804e1eefcf69e4fc6d23e4908
Sidney Alcantara <theisidney@gmail.com>
2017-10-21 03:02:09 +1100

    Version 1.2.0
    - Added mobile nav (yay!) but had to duplicate right items (boo)
    - Title now updates to whatever doc
    - Dropdown text is flexible
    - Moved iframe loading indicator to on top of logo
    - Removed 'HSC Past Papers' text next to logo
    - Cleaned up code & added a lot more code comments
    - Minified main css & js

--- js/hscpapers.js
+++ js/hscpapers.js
@@ -1,34 +1,42 @@
 var jsonData, courseIndex, yearIndex, docLink;
 
-$.getJSON(
-	'https://raw.githubusercontent.com/notseenee/nesappscraper/master/data.json',
-	function(data) {
-		jsonData = data;
+$(document).ready(function(){
+	// activate mobile more-dropdown
+	$('#more-dropdown').dropdown({action:'nothing'});
+	// gets JSON from nesappscraper
+	$.getJSON(
+		'https://raw.githubusercontent.com/notseenee/nesappscraper/master/data.json',
+		function(data) {
+			// write to jsonData
+			jsonData = data;
+			// populate course dropdown
+			populateDropdown(jsonData, 'course_name', '#course-menu', false);
+			// activate course dropdown
+			$('#course-dropdown')
+				.removeClass('loading')
+				.dropdown('show')
+				.dropdown({ selectOnKeydown: false });
+		}
+	);
+});
 
-		parseJSON(jsonData, 'course_name', '#course-menu', false);
-
-		$('#course-dropdown')
-			.removeClass('loading')
-			.dropdown('show')
-			.dropdown({ allowTab: true, selectOnKeydown: false });
-	}
-);
-
-function parseJSON(searchIn, searchFor, pushTo, reverse) {
-	// Get all items into an array
+// parses JSON to populate dropdowns
+function populateDropdown(searchIn, searchFor, pushTo, reverse) {
+	// get all items into an array
 	var items = [];
-
-	// Loop through each item and push to array
+	// loop through each item and push to array
 	for (i = 0; i < searchIn.length; i++) {
-		// Format each
-		var formatted = searchIn[i][searchFor].replace('Hsc', 'HSC');
+		// format each
+		var formatted = searchIn[i][searchFor]
+			.replace('Hsc', 'HSC')
+			.replace('hsc', 'HSC');
 		items.push(formatted);
 	}
 	// sort array
 	items.sort();
-	// reverses
+	// optionally reverses array (for year dropdown)
 	if (reverse) items.reverse();
-	// Clears dropdown
+	// clears dropdown
 	$(pushTo).empty();
 	// loops through items in array and adds to the dropdown
 	for (j = 0; j < items.length; j++) {
@@ -36,81 +44,84 @@ function parseJSON(searchIn, searchFor, pushTo, reverse) {
 	}
 }
 
+// when a course is selected, populate year dropdown
 $('#course-input').change( function() {
+	// add loading spinner to year dropdown
 	$('#year-dropdown').addClass('loading').removeClass('disabled');
-
-	// Loops through each element in json object to find index
+	// loops through each element in json object to find index
 	for (k = 0; k < jsonData.length; k++) {
-		if (jsonData[k]['course_name'].toLowerCase() == $('#course-input')[0].value) {
+		if (jsonData[k]['course_name'].toLowerCase() ==
+			$('#course-input')[0].value) {
 			courseIndex = k;
 			break;
 		}
 	}
-	parseJSON(jsonData[courseIndex].packs, 'year', '#year-menu', true);
-
+	// populates dropdown
+	populateDropdown(jsonData[courseIndex].packs, 'year', '#year-menu', true);
+	// activates year dropdown
 	$('#year-dropdown')
 		.removeClass('loading')
 		.dropdown('restore defaults')
 		.dropdown('show')
-		.dropdown({ allowTab: true, selectOnKeydown: false });
-}
-);
+		.dropdown({ selectOnKeydown: false });
+});
 
+// when a year is selected, populate docs dropdown
 $('#year-input').change( function() {
+	// if year is blank for some reason, ignore
 	if ($(this)[0].value == '') return;
-
+	// add loading spinner to year dropdown
 	$('#doc-dropdown').addClass('loading').removeClass('disabled');
-
-	// Loops through each element in json object to find year index
+	// loops through each element in json object to find year index
 	for (l = 0; l < jsonData[courseIndex].packs.length; l++) {
 		if (jsonData[courseIndex].packs[l]['year'] == $('#year-input')[0].value) {
 			yearIndex = l;
 			break;
 		}
 	}
-	parseJSON(jsonData[courseIndex].packs[yearIndex].docs, 'doc_name', '#doc-menu', false);
-
+	// populates dropdown
+	populateDropdown(jsonData[courseIndex].packs[yearIndex].docs,
+		'doc_name', '#doc-menu', false);
+	// activate doc dropdown
 	$('#doc-dropdown')
 		.removeClass('loading')
 		.dropdown('restore defaults')
-		.dropdown('show')
-		.dropdown({ allowTab: true });
-
-	$('#exampack')
+		.dropdown('show');
+	// activate exam pack buttons and adds link
+	$('.button-exampack')
 		.removeClass('disabled')
 		.attr('href', jsonData[courseIndex].packs[yearIndex]['link']);
-}
-);
+});
 
+// when a doc is selected, open it
 $('#doc-input').change( function(){
+	// if selected doc is blank, ignore
 	if ($(this)[0].value == '') return;
-	// Loops thorugh each doc to find doc index
+	// loops thorugh each doc to find doc index
 	for (m = 0; m < jsonData[courseIndex].packs[yearIndex].docs.length; m++) {
-		if (jsonData[courseIndex].packs[yearIndex].docs[m]['doc_name'].toLowerCase() == $('#doc-input')[0].value) {
+		if (jsonData[courseIndex].packs[yearIndex].docs[m]['doc_name'].toLowerCase()
+			== $('#doc-input')[0].value) {
 			docLink = jsonData[courseIndex].packs[yearIndex].docs[m]['doc_link'];
-			// https fix
-			docLink = docLink.replace('http', 'https');
 			break;
 		}
 	}
-
+	// open in iframe
 	$('#iframe').attr('src', docLink);
+	// add loading indicator on logo
 	$('#iframe-loader').addClass('active');
-	$('#download').removeClass('disabled').attr('href', docLink);
-	$('#link').removeClass('disabled').removeAttr('disabled');
-
+	// activate download & link buttons
+	$('.button-download').removeClass('disabled').attr('href', docLink);
+	$('.button-link').removeClass('disabled').removeAttr('disabled');
+	// change tab title
+	document.title = $('#year-dropdown .text').html() + ' ' +
+		$('#doc-dropdown .text').html() + ' - ' +
+		$('#course-dropdown .text').html();
 });
-
-$('#link').click( function(){
-	prompt('Copy link below:', docLink);
-});
-
-$('#about').click( function(){
-	$('#about-modal').modal('show');
-});
-
-$('#reload').click( function(){ location.reload(); });
-
-$('#iframe').on('load', function(){
-	$('#iframe-loader').removeClass('active');
-});
+// when logo is clicked, reload
+$('#logo').click( function(){ location.reload(); });
+// when link button is clicked, show prompt
+$('.button-link').click( function(){ prompt('Copy link below:', docLink); });
+// when about button is clicked, show modal
+$('.button-about').click( function(){ $('#about-modal').modal('show'); });
+// when iframe finishes loading, remove loading indicator on logo
+$('#iframe').on('load', function(){	$('#iframe-loader').removeClass('active'); });
