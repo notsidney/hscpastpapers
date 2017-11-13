1913acfbb8c7ec6e2ddba7d417c2734091ba6c30
Sidney Alcantara <theisidney@gmail.com>
2017-11-13 19:08:36 +1100

    Added animation

--- js/split.js
+++ js/split.js
@@ -1,12 +1,35 @@
+var courseIndex2, yearIndex, docLink,
+urlCourse2, urlYear2, urlDoc2,
+selectedCourse2, selectedYear2, selectedDoc2;
+
+// if url has ?mode=split, open split view immediately
+$(document).ready(function(){
+	// open about window
+	if (urlParam('mode') == 'split') $('body').addClass('split');
+});
+
 // activate split view
 $('.button-split').click(function(){
 	// add split class to body
 	$('body').addClass('split');
 	// construct new params to keep already open doc, if it exists
-	var splitParams = '?mode=split';
-	if (selectedCourse) splitParams += '&course=' + selectedCourse;
-	if (selectedYear) splitParams += '&year=' + selectedYear;
-	if (selectedDoc) splitParams += '&doc=' + selectedDoc;
+	var newParams = '?mode=split';
+	if (selectedCourse) newParams += '&course=' + selectedCourse;
+	if (selectedYear) newParams += '&year=' + selectedYear;
+	if (selectedDoc) newParams += '&doc=' + selectedDoc;
 	// change url
-	history.pushState(null, '', splitParams);
+	history.pushState(null, '', newParams);
 });
+
+// exit split view
+$('.button-splitexit').click(function(){
+	// remove split class from body
+	$('body').removeClass('split');
+	// construct new params to keep already open doc, if it exists
+	var newParams = '?';
+	if (selectedCourse) newParams += 'course=' + selectedCourse;
+	if (selectedYear) newParams += '&year=' + selectedYear;
+	if (selectedDoc) newParams += '&doc=' + selectedDoc;
+	// change url
+	history.pushState(null, '', newParams);
+})
