858e50817bf9c8d7ca8b873582d71eccd0604292
Sidney Alcantara <theisidney@gmail.com>
2017-12-06 22:34:37 +1100

    Add progress bar on data load

--- js/src/ready.js
+++ js/src/ready.js
@@ -30,45 +30,13 @@ $(document).ready(function(){
 	// activate mobile more-dropdown
 	$('#more-dropdown').dropdown({action:'nothing'});
 	// gets JSON from nesappscraper
-	$.getJSON(
-		'data/data.json',
-		function(data) {
-			// write to jsonData
-			jsonData = data;
-			// populate course dropdown
-			populateDropdown(jsonData, 'course_name', '#course-menu', false);
-			// also populate dropdown for split view
-			populateDropdown(jsonData, 'course_name', '#course-menu2', false);
-			// remove loading indicator
-			$('#loader').removeClass('active');
-			$('#loadingmsg').hide();
-			// activate course dropdown
-			$('#course-dropdown')
-				.removeClass('disabled')
-				.dropdown('show')
-				.dropdown({ selectOnKeydown: false });
-			// also activate course dropdown for split view
-			$('#course-dropdown2')
-				.removeClass('disabled')
-				.dropdown({ selectOnKeydown: false });
-			// Select course from URL parameter
-			if (urlCourse) {
-				$('#course-dropdown')
-					.dropdown('set selected', urlCourse)
-					.dropdown('hide');
-				// if not found
-				if ( !$('#course-dropdown').dropdown('get value') ) urlNotFound('Course');
-			}
-			// Select course2 from URL parameter
-			if (urlCourse2) {
-				$('#course-dropdown2')
-					.dropdown('set selected', urlCourse2)
-					.dropdown('hide');
-				// if not found
-				if ( !$('#course-dropdown2').dropdown('get value') ) urlNotFound('Course2');
-			}
-		}
-	);
+	// $.getJSON('data/data.json', dataReceived)
+	$.ajax({
+		dataType: 'json',
+		url: 'data/data.json',
+		success: dataReceived,
+		xhr: dataProgress
+	});
 	// Get version
 	// show in about modal
 	$('#version').html(version);
@@ -85,3 +53,52 @@ $(document).ready(function(){
 	// Set about modal transition duration
 	$('#about-modal').modal({ duration: 200 });
 });
+
+function dataProgress() {
+	var xhr = new window.XMLHttpRequest();
+	xhr.addEventListener('progress', function(e) {
+		var percent = Math.floor(e.loaded / 1269870 * 100);
+		$('#loadingbar').progress({ percent: percent });
+		$('#loadingpercent').html(percent + '%');
+	}, false);
+	return xhr;
+}
+
+function dataReceived(data) {
+	console.log('received');
+	// write to jsonData variable
+	jsonData = data;
+	// populate course dropdown
+	populateDropdown(jsonData, 'course_name', '#course-menu', false);
+	// also populate dropdown for split view
+	populateDropdown(jsonData, 'course_name', '#course-menu2', false);
+	// remove loading indicator
+	$('#loadingpercent').html('100%');
+	$('#loadingbar').progress({ percent: 100 }).delay(500).fadeOut(200);
+	$('body').removeClass('loading');
+	// activate course dropdown
+	$('#course-dropdown')
+		.removeClass('disabled')
+		.dropdown('show')
+		.dropdown({ selectOnKeydown: false });
+	// also activate course dropdown for split view
+	$('#course-dropdown2')
+		.removeClass('disabled')
+		.dropdown({ selectOnKeydown: false });
+	// Select course from URL parameter
+	if (urlCourse) {
+		$('#course-dropdown')
+			.dropdown('set selected', urlCourse)
+			.dropdown('hide');
+		// if not found
+		if ( !$('#course-dropdown').dropdown('get value') ) urlNotFound('Course');
+	}
+	// Select course2 from URL parameter
+	if (urlCourse2) {
+		$('#course-dropdown2')
+			.dropdown('set selected', urlCourse2)
+			.dropdown('hide');
+		// if not found
+		if ( !$('#course-dropdown2').dropdown('get value') ) urlNotFound('Course2');
+	}
+}
